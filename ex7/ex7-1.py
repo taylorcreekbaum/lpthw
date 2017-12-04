d853754ea56c669d175a9dea002d57e3c8cb2654
Taylor Creekbaum <taylorcreekbaum@gmail.com>
2017-12-03 21:54:53 -0600

    exercise 7 study drills

---
+++ ex7/ex7-1.py
@@ -0,0 +1,39 @@
+# Prints a string
+print("Mary had a little lamb")
+# Prints a formatted string
+print("Its fleece was white as {}.".format('snow'))
+# Prints a string
+print("And everywhere that Mary went.")
+# Prints a string 10 times
+print("." * 10) # what'd that do?
+
+# Assigns a string to the variable end1
+end1 = "C"
+# Assigns a string to the variable end2
+end2 = "h"
+# Assigns a string to the variable end3
+end3 = "e"
+# Assigns a string to the variable end4
+end4 = "e"
+# Assigns a string to the variable end5
+end5 = "s"
+# Assigns a string to the variable end6
+end6 = "e"
+# Assignsa string to the variable end7
+end7 = "B"
+# Assigns a string to the variable end8
+end8 = "u"
+# Assigns a string to the variable end9
+end9 = "r"
+# Assigns a string to the variable end10
+end10 = "g"
+# Assigns a string to the variable end11
+end11 = "e"
+# Assigns a string to the variable end12
+end12 = "r"
+
+# watch end = ' ' at the end. try removing it to see what happens
+# Concatenates end1 - end 6 and prints the result without a newline
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# Concatenates end7 - end 8 and prints the result
+print(end7 + end8 + end9 + end10 + end11 + end12)
