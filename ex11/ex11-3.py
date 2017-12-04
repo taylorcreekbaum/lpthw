9867bd78d1799ef4976784a9d32aec92ab7ba87d
Taylor Creekbaum <taylorcreekbaum@gmail.com>
2017-12-04 07:18:15 -0600

    ex11 study drills

---
+++ ex11/ex11-3.py
@@ -0,0 +1,8 @@
+print("What is your favorite color?", end=' ')
+color = input()
+print("What is your favoriate food?", end=' ')
+food = input()
+print("What is your favorite hobby?", end=' ')
+hobby = input()
+
+print(f"So would you enjoy eating {food} and looking at {color} pictures while {hobby}?")
