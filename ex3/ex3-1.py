c0f993ecd1b7fd32c83373417c301acc0b139531
Taylor Creekbaum <taylorcreekbaum@gmail.com>
2017-12-03 19:35:36 -0600

    Exercise 3 study drills

---
+++ ex3/ex3-1.py
@@ -0,0 +1,39 @@
+# 1. Above each line, use the # to write a comment to yourself explaining what the line does.
+
+# Prints a statement
+print("I will now count my chickens:")
+
+# Prints a statement followed by the result of a mathematical expression
+print("Hens", 25 + 30 / 6)
+# Prints a statement followed by the result of a mathematical expression
+print("Roosters", 100 - 25 * 3 % 4)
+
+# Prints a statement
+print("Now I will count the eggs:")
+
+# Prints the result of a mathematical expression
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Prints a statement
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Prints the result of a mathematical expression
+print(3 + 2 < 5 - 7)
+
+# Prints a statement followed by the result of a mathematical expression
+print("What is 3 + 2?", 3 + 2)
+# Prints a statement followed by the result of a mathematical expression
+print("What is 5 - 7?", 5 - 7)
+
+# Prints a statement
+print("Oh, that's why it's False.")
+
+# Prints a statement
+print("How about some more.")
+
+# Prints a statement followed by the boolean result of a comparison of two mathematical expression
+print("Is it greater?", 5 > -2)
+# Prints a statement followed by the boolean result of a comparison of two mathematical expression
+print("Is it greater or equal?", 5 >= -2)
+# Prints a statement followed by the boolean result of a comparison of two mathematical expression
+print("Is it less or equal?", 5 <= -2)
---
+++ ex3/ex3-3.py
@@ -0,0 +1,6 @@
+# 3. Find something you need to calculate and write a new .py file that does it.
+
+print("How long will it take to save $20000 for a new car?")
+print("I can save $500 a month")
+print("It will take", 20000/500, "months to save the money.")
+print("That's", (20000/500)/12, "years")
