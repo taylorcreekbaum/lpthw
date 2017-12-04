812b9ba4b09aa04aaf5bdf24decd45df9c293d98
Taylor Creekbaum <taylorcreekbaum@gmail.com>
2017-12-03 22:20:23 -0600

    ex9 study drills

--- ex9/ex9-1.py
+++ ex9/ex9-1.py
@@ -1,14 +1,19 @@
 # Here's some new strange stuff, remember to type it exactly.
 
+# Assigns a string to the variable days
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# Assigns a string with embedded newlines to the variable months
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# Prints a string and the value stored in the variable days
 print("Here are the days: ", days)
+# Prints a string and the value stored in the variable months
 print("Here are the months: ", months)
 
+# Prints a triple quoted string
 print("""
 There's something going on here.
 With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
-""")
+"")
