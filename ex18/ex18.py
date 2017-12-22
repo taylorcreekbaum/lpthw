b5fc10b0910174f2a757725b5cd1a115a5ffb94f
Taylor Creekbaum <taylorcreekbaum@gmail.com>
2017-12-22 08:28:54 -0600

    ex18

--- ex18/ex18.py
+++ ex18/ex18.py
@@ -8,7 +8,7 @@ def print_two_again(arg1, arg2):
     print(f"arg1: {arg1}, arg2: {arg2}")
 
 # this just takes one argument
-def print_one(arg1)
+def print_one(arg1):
     print(f"arg1: {arg1}")
 
 # this one takes no arguments
